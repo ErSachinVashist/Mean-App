c53d6d0708f27456816390e8b0bcfa558c77450d
Sachin Vashist <sachin.v@serverguy.com>
2017-11-01 12:59:35 +0530

    first commit

--- config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url : 'mongodb://sachinVashist:sv1282ag12@ds241025.mlab.com:41025/meandb'
-}
--- public/js/constants.js
+++
@@ -1,2 +0,0 @@
-var API_URL="http://localhost:5000/api/";
-//var API_URL="https://eerie-witch-20945.herokuapp.com:5000/api/";
